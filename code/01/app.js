9fd79998fc147e46a65a092215f54dfde9df98d9
Kirta-Linda Karits <31627919+k1rta@users.noreply.github.com>
2025-10-15 13:33:50 +0300

    javascript practice

---
+++ code/01/app.js
@@ -0,0 +1,40 @@
+// External (väline) skript
+// alert('Tere maailm!');
+
+// confirm('Kas sa tahaksid lõunale minna?');
+
+// let firstName = 'Kirta';
+
+// firstName = 'Linda';
+
+// alert(firstName);
+
+// let firstName = prompt('Sisesta oma eesnimi:');
+
+// alert(firstName);
+
+// let choice = confirm('Vali ei või jah?');
+
+// alert(choice);
+
+// const number1 = prompt('Sisesta esimene number:');   
+// const number2 = prompt('Sisesta teine number:');
+
+// alert(number1 - number2);
+
+// let age = 30;
+// alert(age);
+// age += 1;
+// age = age +1;.
+// alert(age);
+
+// const password = prompt('Sisesta parool:');
+// alert(password == 123);
+
+const a = 1;
+const b = 2;
+c = a + b;
+a = 10;
+console.log(c); 
+
+
