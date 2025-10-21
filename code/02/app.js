1a2ec2f5e7cba1502f5abf1fc2c68bf5a9ac4109
Kirta-Linda Karits <31627919+k1rta@users.noreply.github.com>
2025-10-21 13:28:15 +0300

    valgusfoori ül

--- code/02/app.js
+++ code/02/app.js
@@ -1,3 +1,51 @@
-const name = prompt('Sisesta oma nimi');
+// Tervituse programm
 
-alert('Tere ' + name);
+/* const name = prompt('Sisesta oma nimi');
+
+alert('Tere ' + name); */
+
+// Vanuse programm
+
+/* const dateOfBirth = prompt('Sisesta oma sünniaasta');
+
+const currentYear = 2025;
+
+const age = currentYear - dateOfBirth;
+
+alert('Sinu vanus on ' + (age) + ' aastat'); */
+
+/* const pikkus = prompt('Sisesta pikkus: ');
+const laius = prompt('Sisesta laius: ');
+
+const pindala = pikkus * laius;
+
+alert('Ristküliku pindala on ' + pindala + ' ruutmeetrit'); */
+
+/* const celsiusTemp = prompt('Sisesta temperatuur Celsius kraadides: ');
+
+const fahrenheitTemp = celsiusTemp * 1.8 + 32;
+
+alert(celsiusTemp + ' Celsiuse kraadi on ' + fahrenheitTemp + ' Fahrenheiti kraadi'); */
+
+// Teine versioon
+
+/* const C = prompt('Sisesta temperatuur Celsius kraadides: ');
+
+const F = C * 1.8 + 32;
+
+alert('Temperatuur Fahrenheit skaalal on: ' + F); */
+
+// Tingimuslaused
+
+const color = prompt('Mis värvi on foori tuli?');
+
+if (color === 'roheline') {
+    alert('Liigu!');
+} else (color === 'punane'); {
+    alert('Ära liigu!');
+}
+
+
+    
+
+
