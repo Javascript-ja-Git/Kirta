402f3b44a5d02d585eee180f4248c3f17a13dfac
Kirta-Linda Karits <31627919+k1rta@users.noreply.github.com>
2025-12-01 15:42:31 +0200

    kuues loeng osa I

---
+++ code/06/app.js
@@ -0,0 +1,56 @@
+// Massiivi mingi objekti kirjeldamiseks
+
+/* const tvs = [
+    [86, 'Android', 100, 40],
+    [52, 'Apple', 89, 30],
+    [62, 'Sasung', 120, 35]
+];
+
+for(let i = 0; i < tvs.length; i++) {
+    console.log(tvs[i]);
+} */
+
+/* const tv = {
+    diagonal: 52,
+    brand: "Apple",
+    powerConsumtion: 100,
+    weight: 40
+};
+
+const user = {
+    email: "test@test.ee",
+    firstName: "Kirta",
+    lastName: "Karits",
+    yearOfBirth: 1989
+};
+
+console.log(tv.brand); */
+
+/* const tvs = [
+    {
+        diagonal: 52,
+        brand: "Apple",
+        powerConsumtion: 100,
+        weight: 40
+    },
+    {
+        diagonal: 62,
+        brand: "Sasung",
+        powerConsumtion: 120,
+        weight: 35
+    },
+    {
+        diagonal: 86,
+        brand: "Android",
+        powerConsumtion: 100,
+        weight: 40
+    }
+];
+
+for(let i = 0; i < tvs.length; i++) {
+    console.log(tvs[i]);
+}
+
+console.log(tvs[0].weight) */
+
+
